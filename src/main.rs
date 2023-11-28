895e1910627c1f3bb944560f1977d8c1b365cd04
Wei Yang <wei.yang@rwth-aachen.de>
2023-11-28 15:29:44 +0100

    Add Fake error.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 
 mod shuttle_axum;
 
@@ -6,9 +6,15 @@ async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn handle_error() -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong!")
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(handle_error));
 
     Ok(router.into())
 }
