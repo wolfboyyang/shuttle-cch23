22f602840e92b0c8327f6e92ca24b8ae5a00b602
Wei Yang <wei.yang@rwth-aachen.de>
2023-12-01 14:48:17 +0100

    Day 1: Task 2 The sled ID system

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use axum::{
-    extract::Path,
-    http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -10,16 +8,21 @@ async fn handle_error() -> impl IntoResponse {
     (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong!")
 }
 
-async fn cube_the_bits(Path((num1, num2)): Path<(i32, i32)>) -> impl IntoResponse {
-    let result = (num1 ^ num2).pow(3);
-    (StatusCode::OK, result.to_string())
+async fn cube_the_bits(Path(path): Path<String>) -> impl IntoResponse {
+    let cube_bits = path
+        .split('/')
+        .map(|s| s.parse::<i32>().unwrap())
+        .inspect(|x| println!("{}", x.to_string()))
+        .fold(0, |acc, x| acc ^ x);
+    let sled_id = cube_bits.pow(3);
+    (StatusCode::OK, sled_id.to_string())
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/1/:num1/:num2", get(cube_the_bits))
+        .route("/1/*path", get(cube_the_bits))
         .route("/-1/error", get(handle_error));
 
     Ok(router.into())
