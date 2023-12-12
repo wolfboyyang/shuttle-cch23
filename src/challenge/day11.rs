5e8fb6fc9f6115480e9eac76d2312921b86e4869
Wei Yang <wei.yang@rwth-aachen.de>
2023-12-12 12:31:49 +0100

    Day 11: Imagery from the North Pole

--- src/challenge/day11.rs
+++ src/challenge/day11.rs
@@ -7,13 +7,26 @@ pub fn task() -> Router {
         .route("/red_pixels", post(activate_bull_mode))
 }
 
-async fn activate_bull_mode(mut multipart: Multipart) {
+async fn activate_bull_mode(mut multipart: Multipart) -> String {
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
+        if name != "image" {
+            continue;
+        }
         let data = field.bytes().await.unwrap();
 
-        println!("Length of `{}` is {} bytes", name, data.len());
+        return match lodepng::decode_memory(data, lodepng::ColorType::RGBA, 8) {
+            Ok(lodepng::Image::RGBA(image)) => image
+                .buffer
+                .iter()
+                .filter(|pixel| pixel.r as u16 > pixel.g as u16 + pixel.b as u16)
+                .count()
+                .to_string(),
+            Ok(_) => "Decoded image, but it was not RGBA".into(),
+            Err(reason) => format!("Could not load, because: {reason}").into(),
+        };
     }
+    "No image found".into()
 }
 
 #[cfg(test)]
@@ -30,23 +43,22 @@ mod tests {
         let server = TestServer::new(app).unwrap();
 
         // Send the request.
-        let response = server.get("/").await;
+        let response = server.get("/assets/decoration.png").await;
 
         response.assert_status(StatusCode::OK);
 
-        response.assert_text("Hello, world!");
-    }
+        assert!(response
+            .headers()
+            .get("content-type")
+            .is_some_and(|v| v == "image/png"));
+        assert!(response
+            .headers()
+            .get("content-length")
+            .is_some_and(|v| v == "787297"));
 
-    #[tokio::test]
-    async fn task2() {
-        let app = task();
+        let bytes = response.as_bytes();
+        const EXPECTED: &[u8] = include_bytes!("../../assets/decoration.png");
 
-        // Run the application for testing.
-        let server = TestServer::new(app).unwrap();
-
-        // Send the request.
-        let response = server.get("/-1/error").await;
-
-        response.assert_status(StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(bytes, EXPECTED);
     }
 }
